b60a4608cb96c6ed7e02e896dfa6bec41445be97
venkawu <45788624+venkawu@users.noreply.github.com>
2021-12-24 20:48:39 -0600

    Add files via upload

---
+++ McAfee-jdbc-app/src/main/java/com/epo/gccr/McAfeejdbcapp/McAfeeJdbcAppApplication.java
@@ -0,0 +1,12 @@
+package com.epo.gccr.McAfeejdbcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAfeeJdbcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McAfeeJdbcAppApplication.class, args);
+	}
+}
